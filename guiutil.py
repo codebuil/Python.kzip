136ba4b8c9f5020b8e342a426bcb2d182fd7328d
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-10-23 16:08:34 +0100

    Add files via upload

--- guiutil.py
+++ guiutil.py
@@ -83,37 +83,43 @@ def decompress_file():
 def extract_files():
     packed_file = filedialog.askopenfilename(title="Selecione o arquivo empacotado")
     output_directory = filedialog.askdirectory(title="Selecione o diretório de saída")
-    if packed_file and output_directory:
-        with open(packed_file, 'rb') as pak_file:
-            pak_file.seek(-struct.calcsize('Q'), 2)
-            list_offset = struct.unpack('Q', pak_file.read(struct.calcsize('Q')))[0]
-            pak_file.seek(list_offset)
-            list_data = pak_file.read()
-            list_entries = list_data.decode('utf-8').split('\n')
+    with open(packed_file, 'rb') as pak_file:
+        # Leitura do deslocamento da lista a partir do final do arquivo
+        pak_file.seek(-struct.calcsize('Q'), 2)
+        list_offset = struct.unpack('Q', pak_file.read(struct.calcsize('Q')))[0]
 
-            for i in range(len(list_entries)):
-                entry = list_entries[i]
-                if entry:
-                    entrys = entry.split(',')
-                    if len(entrys) > 1:
-                        file_name = entrys[0]
-                        start_offset = int(entrys[1])
+        # Lê a lista de arquivos a partir do deslocamento
+        pak_file.seek(list_offset)
+        list_data = pak_file.read()
+        list_entries = list_data.decode('utf-8').split('\n')
 
-                        if i < len(list_entries) - 1:
-                            next_entry = list_entries[i + 1]
-                            next_start_offset = int(next_entry.split(',')[1])
-                            file_size = next_start_offset - start_offset
-                        else:
-                            pak_file.seek(0)
-                            file_size = list_offset
+        for i in range(len(list_entries)):
+            entry = list_entries[i]
+            if entry:
+                entrys = entry.split(',')
+                if len(entrys) > 1:
+                    file_name = entrys[0]
+                    start_offset = int(entrys[1])
 
-                        pak_file.seek(start_offset)
-                        file_data = pak_file.read(file_size)
+                    # Calcula o tamanho do arquivo atual com base no próximo arquivo na lista
+                    if i < len(list_entries) - 2:
+                        
+                        next_entry = list_entries[i + 1]
+                        next_start_offset = int(next_entry.split(',')[1])
+                        file_size = next_start_offset - start_offset
+                    else:
+                        # No último arquivo, extrai até o início do arquivo "list.txt"
+                        pak_file.seek(0)
+                        file_size = list_offset - start_offset
 
-                        with open(f"{output_directory}/{file_name}", 'wb') as output_file:
-                            output_file.write(file_data)
+                    # Lê o conteúdo do arquivo do pacote
+                    pak_file.seek(start_offset)
+                    file_data = pak_file.read(file_size)
 
-        messagebox.showinfo("Sucesso", "Arquivos extraídos com sucesso.")
+                    # Escreve o arquivo descompactado
+                    with open(f"{output_directory}/{file_name}", 'wb') as output_file:
+                        output_file.write(file_data)
+
 
 # Configuração da janela principal
 root = tk.Tk()
